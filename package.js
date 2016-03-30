829c7f5088b3303a9d3d94e91fdcad4d1fbf5c5f
barbatus <alex.borodach@gmail.com>
2016-03-30 23:37:32 +0300

    Bump version, add README.

--- package.js
+++ package.js
@@ -1,12 +1,14 @@
 Package.describe({
   name: 'barbatus:typescript-runtime',
-  version: '0.1.0-beta.1',
+  version: '0.1.0',
   summary: 'TypeScript Runtime for Meteor',
   git: 'https://github.com/barbatus/typescript-runtime',
   documentation: null
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+
   api.addFiles(['typescript-helpers.js']);
 
   api.export(['__extends', '__decorate', '__metadata', '__param', '__awaiter']);
